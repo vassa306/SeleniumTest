77d64b73b99cc01d9b62922100fb6afff35ea98a
vj <vaclav.jurena@gmail.com>
2020-07-20 21:37:40 +0200

    testng.xml

--- src/test/java/constants/SystemConstants.java
+++ src/test/java/constants/SystemConstants.java
@@ -4,6 +4,7 @@ public class SystemConstants {
 
        public static String DEFAULT_LOGIN_NAME = "";
        public static int PAGELOADING = 5000;
+       public static int DROPDOWNTIMEOUT = 300;
 
 
 }
